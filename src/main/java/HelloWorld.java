f804af3a38f4e9f4902bd476f83873eff865263c
Denys Tyryshkin <122382157+tyryshkin1997@users.noreply.github.com>
2023-01-23 22:20:08 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // just add text on gitHub web for test
+        System.out.println("‘Hello mates!");
     }
 }
